814c975ea280fc989945f91314de368b6a05ca55
Sahil <sahillearn44@gmail.com>
2024-07-20 20:49:29 +0530

    ok

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Mycom from './mycom.jsx'
 
+function Myele() {
+    return(
+        <>
+        <div>
+            <h2>Hello world</h2>
+            <p>this is main element or component</p>
+        </div>
+        </>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <>
  <Mycom/> 
     <App/>
+    <Myele/>
     
     </>
 )
