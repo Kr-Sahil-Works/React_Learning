865f24ea123fd8aa337da05607b314a4ece3d8ae
Sahil <sahillearn44@gmail.com>
2024-07-24 15:50:59 +0530

    ok

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,15 +1,28 @@
+import Card from "./components/Card";
+
 function App() {
+  
+  let simpleobj={
+    name:"Nobita",
+    age: 18
+  }
+  let myarr=[1,2,3,4,5]
+  
+  let imgsrc1 ="https://pm1.aminoapps.com/7629/1b0f0066f5b6afe4c60d82d140416ff11e3c5447r1-720-720v2_hq.jpg";
+  let imgsrc2="https://avatars.githubusercontent.com/u/29142494?v=4";
+  
+
   return (
     <>
 <h1 className='text-[#ffffff] bg-green-400 p-5 rounded-md font-semibold italic'>Hello working</h1>
 
-<figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800" >
+<figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800" >
 
-  <img class="w-24 h-24 rounded-full mx-auto" src="https://pm1.aminoapps.com/7629/1b0f0066f5b6afe4c60d82d140416ff11e3c5447r1-720-720v2_hq.jpg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 rounded-full mx-auto" src="https://pm1.aminoapps.com/7629/1b0f0066f5b6afe4c60d82d140416ff11e3c5447r1-720-720v2_hq.jpg" alt="" width="384" height="512"/>
 
-  <div class="pt-6 space-y-4" >
+  <div className="pt-6 space-y-4" >
     <blockquote>
-      <p class="text-lg font-medium">
+      <p className="text-lg font-medium">
         <span className="text-[#05f2ea]">“Tailwind CSS </span> is the only framework that I've seen scale
         on large teams. It’s easy to customize, adapts to any design,
         and the build size is tiny.”
@@ -17,38 +30,26 @@ function App() {
     </blockquote>
   </div>
 
-  <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
+  <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
         Nobita Nobi
       </div>
 
-      <div class="text">
+      <div className="text">
         Full stack Web Developer, Bangalore
       </div>
   </figcaption>
   
 </figure>
 
-{/* ________________________________________ */}
+<div className="flex gap-5 ml-5 my-10">
 
-<div class="relative h-[400px] w-[300px] rounded-md">
-  <img
-    src="https://pm1.aminoapps.com/7629/1b0f0066f5b6afe4c60d82d140416ff11e3c5447r1-720-720v2_hq.jpg"
-    alt="Profile_image"
-    class="z-0 h-full w-full rounded-[10px] object-cover"/>
+{/* <Card username ="Akkia" simpleobj={simpleobj} />
+<Card username ="Nobita" myarr={myarr} /> */}
 
+<Card username ="Akkia" btntext="vist me ->" image_var={imgsrc1} />
+<Card username ="Nobita" btntext="click me" image_var={imgsrc2} />
 
-  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-
-  <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Nobita</h1>
-    <p class="mt-2 text-sm text-gray-300">
-     He is html,css,javascript and react learner.
-    </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
-    </button>
-  </div>
 </div>
 
     </>
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// function Card(props) {
+function Card({username,btntext,image_var}) {
+    console.log("username",username);
+    console.log("button_text",btntext);
+    return (
+<div className="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src={image_var}
+    alt="Profile_image"
+    className="z-0 h-full w-full rounded-[10px] object-cover"/>
+
+
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">{username}</h1>
+    <p className="mt-2 text-sm text-gray-300">
+     He is html,css,javascript and react learner.
+    </p>
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      {/* View Profile → */}
+      {btntext}
+    </button>
+  </div>
+</div>
+    )
+}
+
+export default Card
