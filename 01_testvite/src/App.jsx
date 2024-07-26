bf87a2a29fb3970269b4d81b01a32aef4d3af601
Sahil <sahillearn44@gmail.com>
2024-07-26 18:49:14 +0530

    ok

---
+++ 01_testvite/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>working now !</h1>
+    </>
+  )
+}
+
+export default App
--- 02_Hooks/src/App.jsx
+++ 02_Hooks/src/App.jsx
@@ -1,12 +1,43 @@
+import React,{useState} from "react";
+
 function App() {
-  let counter = 9; 
+
+let [counter,setCounter] = useState(9)
+// let counter = 9; // This variable cannot propogate in UI by updating.
+
+  function addvalue() {
+    if (counter < 20) {
+      counter = counter + 1
+    setCounter(counter)
+    console.log("clicked and value is now : ",{counter})
+    }
+    else{
+      console.log("value reached its limit :(  ");
+    }
+  }
+
+  const revvalue= ()=>{ 
+    if (counter > 0) {
+      counter = counter - 1
+    setCounter(counter)
+    console.log("clicked and value is now : ",{counter}) 
+    }
+    else{
+      console.log("value reached its limit :(  ");
+    }
+  }
+
+
   return (
     <>
-    <h1 className="text-white">React and Hooks concepts</h1>
-      <button onClick={counter}>ADD ME</button>
-      <button onClick={counter}>REMOVE ME</button>
-      <p>The value is now:{counter}</p>
-      <footer>Finally the value of counter is ; {counter}</footer>
+    <div className="flex flex-col m-auto p-80  ">
+    <h1 className="font-bold text-[40px] text-center">React and Hooks concepts</h1>
+      <button onClick={addvalue}>ADD ME → {counter}</button>
+      <button onClick={revvalue}>REMOVE ME → {counter}</button>
+      <p className="mt-5 font-semibold">The value is now : {counter}</p>
+      <br />
+      <footer className="mt-[-15px] font-semibold">Finally the value of counter is : {counter}</footer>
+      </div>
     </>
   )
 }
